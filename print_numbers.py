7836edb8e07b899b4805ceecd97208c9cafc825a
MarikaW <104352162+jewe6300@users.noreply.github.com>
2025-03-10 21:24:14 +0100

    Adding python script print numbers

---
+++ print_numbers.py
@@ -0,0 +1,9 @@
+def print_numbers(start, stop):
+	for number in range(start, stop + 1):
+		print (number)
+
+a = int(input('Enter start value: '))
+
+b = int(input('Enter stop value: '))
+
+print_numbers(a, b)
